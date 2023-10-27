c50210ea1ccdba7f2e63cdc2062741f524335d94
Arya Priema <99aryaprima@gmail.com>
2023-10-28 00:36:31 +0700

    fix: services/token.service.js - date and code issue

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -3,13 +3,20 @@ import jwt from 'jsonwebtoken';
 import moment from 'moment';
 import tokenTypes from '../config/tokens.config.js';
 
-export const generateToken = async (user, secret, expired) => {
+export const generateToken = async ({ user, secret, expired, type }) => {
   try {
     if (!user) throw new Error('User not found');
     if (!secret) throw new Error('Secret not found');
     if (!expired) throw new Error('Expired not found');
 
-    return jwt.sign(user, secret, { expiresIn: expired });
+    const payload = {
+      sub: user,
+      iat: moment().unix(),
+      exp: expired.unix(),
+      type,
+    };
+
+    return jwt.sign(payload, secret);
   } catch (error) {
     console.info('[ERROR_GENERATE_TOKEN]', error);
   }
@@ -27,13 +34,12 @@ export const saveToken = async ({
     if (!userId) throw new Error('userId not found');
     if (!expired) throw new Error('expired not found');
     if (!type) throw new Error('type not found');
-    if (!blacklisted) throw new Error('blacklisted not found');
 
     const tokenDoc = await db.token.create({
       data: {
         token,
         userId,
-        expires: expired,
+        expires: expired.toDate(),
         type,
         blacklisted,
       },
@@ -46,39 +52,48 @@ export const saveToken = async ({
 };
 
 export const generateAuthTokens = async (user) => {
-  const { name, username, id } = user;
+  const { name, email, id } = user;
 
   if (!name) throw new Error('name not found');
-  if (!username) throw new Error('username not found');
+  if (!email) throw new Error('email not found');
   if (!id) throw new Error('id not found');
 
   const dataUser = {
     userId: id,
     name,
-    username,
+    email,
   };
 
   const accessTokenExpires = moment().add(
     process.env.ACCESS_TOKEN_EXPIRED,
     'minutes'
   );
-  const accessToken = generateToken(
-    dataUser,
-    accessTokenExpires,
-    tokenTypes.ACCESS
-  );
+  const accessToken = await generateToken({
+    user: dataUser,
+    secret: process.env.ACCESS_TOKEN_SECRET,
+    expired: accessTokenExpires,
+    type: tokenTypes.ACCESS,
+  });
 
   const refreshTokenExpires = moment().add(
     process.env.REFRESH_TOKEN_EXPIRED,
     'days'
   );
-  const refreshToken = generateToken(
-    dataUser,
-    refreshTokenExpires,
-    tokenTypes.REFRESH
-  );
+  const refreshToken = await generateToken({
+    user: dataUser,
+    secret: process.env.REFRESH_TOKEN_SECRET,
+    expired: refreshTokenExpires,
+    type: tokenTypes.REFRESH,
+  });
 
-  await saveToken(refreshToken, id, refreshTokenExpires, tokenTypes.REFRESH);
+  console.info(refreshToken);
+
+  await saveToken({
+    token: refreshToken,
+    userId: id,
+    expired: refreshTokenExpires,
+    type: tokenTypes.REFRESH,
+  });
 
   return {
     access: {
